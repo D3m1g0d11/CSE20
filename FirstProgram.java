ab12d9fa16737e1e3cc201d4bb3ad4746fc635e8
John Biton <80254416+D3m1g0d11@users.noreply.github.com>
2023-02-20 10:33:06 -0800

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+		System.out.println("world");
+		System.out.print("My name is Brenda Moran");
+		System.out.println("Welcome to CSE20 at UC Merced");
+	}
+
+}
